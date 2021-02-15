1f01a7f3e178c2e5c54de06f291eadac9659d34a
Albert-Wu-03 <22qwu@athenian.org>
2021-02-15 06:10:02 +0000

    HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,16 +4,53 @@ public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        ArrayList<Integer> insertion = new ArrayList<Integer>();
+        insertion.add(list.get(0));
+        for(int i = 1; i < list.size(); i++){
+            if(list.get(i) < insertion.get(0)){
+                insertion.add(0, list.get(i));
+            }else{
+                for(int j = 0; j < insertion.size(); j++){
+                    if(insertion.get(j)>list.get(i)){
+                        insertion.add(j, list.get(i));
+                    }
+                }
+                insertion.add(list.get(i));
+            }
+        }
+            
+        return insertion;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int[] arr = new int[arr1.length + arr2.length];
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while(a < arr1.length && b < arr2.length){
+            if(arr1[a] > arr2[b]){
+                arr[c] = arr2[b];
+                b++;
+            }else{
+                arr[c] = arr1[a];
+                a++;
+            }
+            c++;
+        }
+        while(a < arr1.length){
+            arr[c] = arr1[a];
+            a++;
+            c++;
+        }
+        while(b < arr2.length){
+            arr[c] = arr2[b];
+            b++;
+            c++;
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
